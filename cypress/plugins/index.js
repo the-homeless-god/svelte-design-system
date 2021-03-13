df579ecf2ccd0ab98346b4814ab0864d3c293af2
Zimtir <zimtir@mail.ru>
2021-03-14 02:19:26 +0300

    feat: configuration of cypress

---
+++ cypress/plugins/index.js
@@ -0,0 +1,13 @@
+const rollupPreprocessor = require('@bahmutov/cy-rollup')
+const codeCoverage = require('@cypress/code-coverage/task')
+
+module.exports = (on, config) => {
+  const options = {
+    configFile: './bundlers/rollup.js',
+  }
+
+  on('file:preprocessor', rollupPreprocessor(options))
+  codeCoverage(on, config)
+
+  return config
+}
