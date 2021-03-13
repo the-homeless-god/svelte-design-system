6b65726e8a99667656ea431e259e10bab0134e7a
Zimtir <zimtir@mail.ru>
2021-03-14 02:48:27 +0300

    refactor: improvement of code-style by self-review

--- bundlers/svelte.js
+++ bundlers/svelte.js
@@ -11,10 +11,8 @@ const preprocessor = preprocess({
         '@babel/preset-env',
         {
           loose: true,
-          // No need for babel to resolve modules
           modules: false,
           targets: {
-            // ! Very important. Target es6+
             esmodules: true,
           },
         },
